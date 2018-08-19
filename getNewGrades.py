34d5b4b819acc4ee2756e34dc524b231f4b0611e
arnav-t <avznav@gmail.com>
2018-08-19 15:22:16 +0530

    Implemented multiprocessing to speed up scraping Scraping is much faster now. Progress bar doesn't work anymore, though.

--- getNewGrades.py
+++ getNewGrades.py
@@ -1,12 +1,14 @@
 from subprocess import check_output
 import re
 import json
-from tqdm import tqdm
-import threading
+# from tqdm import tqdm
+# import threading
+import multiprocessing as mp
 import urllib2
 
-cookie = '1265E71286E3CB350AA1343E6D2D3D62.worker2'
-NUM_THREADS = 1
+cookie = 'E22EF71BD356D90657D118D65959BC56.worker2'
+NUM_PROCS = 32
+
 
 def load(file):
     def convert(input):
@@ -28,13 +30,20 @@ def save(dic, name):
 opener = urllib2.build_opener()
 opener.addheaders.append(('Cookie', 'JSESSIONID={}'.format(cookie)))
         
-def getGrades(code):
+def getGrades(params):
+    q, code = params[0], params[1]
     f = opener.open("https://erp.iitkgp.ac.in/Acad/Pre_Registration/subject_grade_status.jsp?subno={}".format(code))
     nums = re.findall('([A-Z\s]+)\(No. of Student\) : ([0-9]+)', f.read())
     if len(nums)!=0:
-        return {str(d[0].strip()): int(d[1]) for d in nums}
+        res = {str(d[0].strip()): int(d[1]) for d in nums}
     else:
-        return {}
+        res = {}
+
+    if len(res) != 0:
+		grades = uniformizeGradesJSON(res)
+		grades.pop('X', None)
+		q.put( {code : {'grades': grades}} )
+		print "Fetched: " + str(q.qsize())
 
 def uniformizeGradesJSON(stats):
     for i in ['EX','A','B','C','D','P','F']:
@@ -42,34 +51,27 @@ def uniformizeGradesJSON(stats):
             stats[i] = 0
     return stats
 
-n = 0
 def main():
     newGrades = {}
     allcourses = load('courses.json').keys()
-    class ScrapingThread(threading.Thread): # Call it Iron-Man thread
+    class ScrapingPool(): # Call it Iron-Man pool
         def __init__(self,courses):
-            super(ScrapingThread, self).__init__()
+            self.pool = mp.Pool(processes=NUM_PROCS)
+            self.man = mp.Manager()
+            self.q = self.man.Queue()
             self.courses=courses
         def run(self):
-            global n
-            for i in tqdm(self.courses):
-                g = getGrades(i)
-                if len(g)!=0:
-                    newGrades[i] = {'grades': uniformizeGradesJSON(g)}
-                    # print i, newGrades[i]
-                n += 1
-    threads = []
-    one_part_len = len(allcourses)/NUM_THREADS
-    for i in range(NUM_THREADS):
-        threads.append(ScrapingThread(allcourses[i * one_part_len : (i+1) * one_part_len]))
-    for i in threads:
-        i.start()
-    for i in threads:
-        i.join()
+        	self.pool.map(getGrades, [(self.q, code) for code in self.courses])
+        	self.pool.close()
+        	self.pool.join()
 
-    for i in newGrades:
-        newGrades[i]['grades'].pop('X',None)
-        
+    sp = ScrapingPool(allcourses)
+    sp.run()
+
+    while not sp.q.empty():
+    	entry = sp.q.get()
+    	newGrades.update(entry)
+
     save(newGrades, 'newGrades')
 
 if __name__ == '__main__':
