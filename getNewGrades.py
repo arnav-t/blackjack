810ae19ab6f23ba1f3ddb1436f3c3bb91ff8395c
arnav-t <avznav@gmail.com>
2018-08-20 12:00:33 +0530

    Record time

--- getNewGrades.py
+++ getNewGrades.py
@@ -5,8 +5,9 @@ import json
 # import threading
 import multiprocessing as mp
 import urllib2
+import time
 
-cookie = 'E22EF71BD356D90657D118D65959BC56.worker2'
+cookie = 'A9E69ED40E56337D0E967F2E8C094704.worker2'
 NUM_PROCS = 32
 
 
@@ -31,19 +32,21 @@ opener = urllib2.build_opener()
 opener.addheaders.append(('Cookie', 'JSESSIONID={}'.format(cookie)))
         
 def getGrades(params):
-    q, code = params[0], params[1]
-    f = opener.open("https://erp.iitkgp.ac.in/Acad/Pre_Registration/subject_grade_status.jsp?subno={}".format(code))
-    nums = re.findall('([A-Z\s]+)\(No. of Student\) : ([0-9]+)', f.read())
-    if len(nums)!=0:
-        res = {str(d[0].strip()): int(d[1]) for d in nums}
-    else:
-        res = {}
+	stud_time = time.time()
+	q, code = params[0], params[1]
+	f = opener.open("https://erp.iitkgp.ac.in/Acad/Pre_Registration/subject_grade_status.jsp?subno={}".format(code))
+	nums = re.findall('([A-Z\s]+)\(No. of Student\) : ([0-9]+)', f.read())
+	if len(nums)!=0:
+		res = {str(d[0].strip()): int(d[1]) for d in nums}
+	else:
+		res = {}
 
-    if len(res) != 0:
+	if len(res) != 0:
 		grades = uniformizeGradesJSON(res)
 		grades.pop('X', None)
 		q.put( {code : {'grades': grades}} )
-		print "Fetched: " + str(q.qsize())
+		stud_delta = time.time() - stud_time
+		print "Fetched: " + str(q.qsize())  + " [ " + ('%.2f' % (stud_delta)) + " ]"
 
 def uniformizeGradesJSON(stats):
     for i in ['EX','A','B','C','D','P','F']:
@@ -52,27 +55,30 @@ def uniformizeGradesJSON(stats):
     return stats
 
 def main():
-    newGrades = {}
-    allcourses = load('courses.json').keys()
-    class ScrapingPool(): # Call it Iron-Man pool
-        def __init__(self,courses):
-            self.pool = mp.Pool(processes=NUM_PROCS)
-            self.man = mp.Manager()
-            self.q = self.man.Queue()
-            self.courses=courses
-        def run(self):
-        	self.pool.map(getGrades, [(self.q, code) for code in self.courses])
-        	self.pool.close()
-        	self.pool.join()
+	start_time = time.time()
+	newGrades = {}
+	allcourses = load('courses.json').keys()
+	class ScrapingPool(): # Call it Iron-Man pool
+		def __init__(self,courses):
+			self.pool = mp.Pool(processes=NUM_PROCS)
+			self.man = mp.Manager()
+			self.q = self.man.Queue()
+			self.courses=courses
+		def run(self):
+			self.pool.map(getGrades, [(self.q, code) for code in self.courses])
+			self.pool.close()
+			self.pool.join()
 
-    sp = ScrapingPool(allcourses)
-    sp.run()
+	sp = ScrapingPool(allcourses)
+	sp.run()
 
-    while not sp.q.empty():
-    	entry = sp.q.get()
-    	newGrades.update(entry)
+	while not sp.q.empty():
+		entry = sp.q.get()
+		newGrades.update(entry)
 
-    save(newGrades, 'newGrades')
+	save(newGrades, 'newGrades')
+	delta_time = time.time() - start_time
+	print "\n\nTotal time: " + ('%.2f' % delta_time)
 
 if __name__ == '__main__':
     main()
